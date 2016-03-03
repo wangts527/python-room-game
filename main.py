2e32ecc33fb2d8d8daf560af77925c1402d16c4d
Wang an <qietingfengyin527@163.com>
2016-03-03 09:06:54 +0800

    第一次更新
    添加文件

---
+++ main.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+
+print " "
+
+choice = raw_input("你要选择哪扇门？\n>")
---
+++ rooms.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+
+def room_none(player=[]):
+    print "这个房间里什么都没有，选择一扇门出去吧."
+    choice = raw_input(">")
